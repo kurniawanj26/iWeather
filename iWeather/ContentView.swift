c92735a43d9be2a0b2fa8da7a2b3364a9302cade
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2025-01-17 21:38:36 +0700

    Initial Commit

---
+++ iWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iWeather
+//
+//  Created by Jayadi Kurniawan on 17/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
